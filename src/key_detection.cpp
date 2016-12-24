efbb72a620ff2879567de9cb3cca0529c053f5a1
shriram <sbshriram16@gmail.com>
2016-12-24 13:27:09 +0530

    Remove two or more contours detected for the same key edge

--- src/key_detection.cpp
+++ src/key_detection.cpp
@@ -17,7 +17,7 @@ using std::ostringstream;
 using namespace cv;
 
 
-void get_gap_pos(vector<Rect> &black_keys_boundRect, vector<int> &gap_pos) {
+void gapPosition(vector<Rect> &black_keys_boundRect, vector<int> &gap_pos) {
 	int avg_black_dist = (black_keys_boundRect[black_keys_boundRect.size() - 1].x - black_keys_boundRect[0].x) / (black_keys_boundRect.size() - 1);
 	for (int i = 0, n = black_keys_boundRect.size() - 1; i < n; i++) {
 		if (black_keys_boundRect[i + 1].x - black_keys_boundRect[i].x > avg_black_dist)
@@ -27,24 +27,24 @@ void get_gap_pos(vector<Rect> &black_keys_boundRect, vector<int> &gap_pos) {
 }
 
 
-int get_median(vector<int> &list) {
+int median(vector<int> &list) {
 	sort(list.begin(), list.end());
 	int l = list.size();
 	return (l % 2 == 0 ? (list[l/2] + list[l/2 + 1])/2 : list[(l+1)/2]);
 }
 
-int get_median_error(vector<int> &list, int list_median) {
+int medianDev(vector<int> &list, int list_median) {
 	int l = list.size();
 	vector<int> list_dev(l);
 	for (int i = 0; i < l; i++) {
 		list_dev[i] = abs(list[i] - list_median);
 	}
-	return get_median(list_dev);
+	return median(list_dev);
 }
 
 
 // add/change improperly detected contours
-void modify_contours(int pos, int x, int uy, int ly, vvP &contours, vector<Rect> &boundRect, vector<Moments> &mu, vector<Point2f> &mc, bool add_new) {
+void modifyContours(int pos, int x, int uy, int ly, vvP &contours, vector<Rect> &boundRect, vector<Moments> &mu, vector<Point2f> &mc, bool add_new) {
 	vector<Point> temp;
 	temp.push_back(Point(x, uy));
 	temp.push_back(Point(x, ly));
@@ -63,68 +63,8 @@ void modify_contours(int pos, int x, int uy, int ly, vvP &contours, vector<Rect>
 	}
 }
 
-
-void modify_detected(vvP &contours, vector<Rect> &boundRect, vector<Moments> &mu, vector<Point2f> &mc, int n_white_keys, Mat &bg) {
-	// getting moments and center of mass of detected contours
-	for (int i = 0, n = contours.size(); i < n; i++) {
-		mu[i] = moments(contours[i], false);
-		mc[i] = Point2f(mu[i].m10/mu[i].m00, mu[i].m01/mu[i].m00);
-	}
-
-	Size s = bg.size();
-
-	// adding white key contours at the edge of the picture if not present
-	int avg_white_lower_y = 0;
-	for (int i = 0, n = boundRect.size(); i < n; i++)
-		avg_white_lower_y += (boundRect[i].y + boundRect[i].height);
-	avg_white_lower_y /= boundRect.size();
-	if (boundRect[0].x > s.width/100) 
-		modify_contours(0, 0, 0, avg_white_lower_y, contours, boundRect, mu, mc, true);
-	if (boundRect[boundRect.size() - 1].x < 99*s.width/100)
-		modify_contours(contours.size(), s.width - 1, 0, avg_white_lower_y, contours, boundRect, mu, mc, true);
-
-	// getting median in y coordinate of edges detected and average deviation from median
-	vector<int> u_y, l_y, dist;
-	for (int i = 0, n = contours.size(); i < n; i++) {
-		u_y.push_back(boundRect[i].y);
-		l_y.push_back(boundRect[i].y + boundRect[i].height);
-	}
-	for (int i = 0, n = contours.size(); i < n - 1; i++)
-		dist.push_back(mc[i + 1].x - mc[i].x);
-
-	int u_y_median = get_median(u_y);
-	int l_y_median = get_median(l_y);
-	int dist_median = get_median(dist); 
-	int u_y_error_median = get_median_error(u_y, u_y_median);
-	int l_y_error_median = get_median_error(l_y, l_y_median);
-
-
-	// correcting detected edges vertically
-	for (int i = 0, n = contours.size(); i < n; i++)
-		if (abs(boundRect[i].y - u_y_median) > 10*u_y_error_median || abs(boundRect[i].y + boundRect[i].height - l_y_median) > 10*l_y_error_median)	
-			modify_contours(i, mc[i].x, u_y_median, l_y_median, contours, boundRect, mu, mc, false);
-
-	Mat center_white_edges = bg.clone();
-	for (int i = 0, n = mc.size(); i < n; i++) {
-		circle(center_white_edges, mc[i], 3, Scalar(0,0,255));
-		// imshow("center_white_edges", center_white_edges);
-		// waitKey(0);
-	}
-	imshow("center_white_edges", center_white_edges);
-	waitKey(0);
-
-	// adding undetected edges of white keys
-	int i = 0;
-	while (i < n_white_keys) {
-		if (abs(mc[i + 1].x - mc[i].x) > 3.0*dist_median/2)
-			modify_contours(i + 1, mc[i].x + dist_median, u_y_median, l_y_median, contours, boundRect, mu, mc, true);
-		i++;
-	}
-}
-
-
 // sorts contours from left to right using information from boundRect - uses selection sort
-void sort_contours(vvP &contours, vector<Rect> &boundRect) {
+void sortContours(vvP &contours, vector<Rect> &boundRect) {
 	for (int i = 0, n = contours.size(); i < n; i++) {
 		int minIndex = i;
 		for (int j = i + 1; j < n; j++) {
@@ -140,7 +80,83 @@ void sort_contours(vvP &contours, vector<Rect> &boundRect) {
 	}
 }
 
-void get_black_keys(Mat &bg, vvP &black_keys, vector<Rect> &black_keys_boundRect) {
+void refineDetected(vvP &contours, vector<Rect> &boundRect, vector<Moments> &mu, vector<Point2f> &mc, int n_white_keys, Mat &bg) {
+	Size s = bg.size();
+
+	// removing false positive white keys detected
+	Mat bg_lower_contour = bg.clone();
+	for (int i = 0, n = contours.size(), j = 0; i < n; i++) {
+		if (boundRect[j].height < s.height/6 || boundRect[j].width > s.width/10) {
+			boundRect.erase(boundRect.begin() + j);
+			contours.erase(contours.begin() + j);
+		}
+		else
+			j++;
+	}
+	sortContours(contours, boundRect);
+
+	// getting moments and center of mass of detected contours
+	for (int i = 0, n = contours.size(); i < n; i++) {
+		mu[i] = moments(contours[i], false);
+		mc[i] = Point2f(mu[i].m10/mu[i].m00, mu[i].m01/mu[i].m00);
+	}
+
+	// removing two very close contour
+	for (int i = 0, n = contours.size(), j = 0; i < n; i++) {
+		if (mc[j + 1].x - mc[j].x < s.width/100) {
+			contours.erase(contours.begin() + j + 1);
+			boundRect.erase(boundRect.begin() + j + 1);
+			mu.erase(mu.begin() + j + 1);
+			mc.erase(mc.begin() + j + 1);
+		}
+		else
+			j++;
+	}
+
+	// adding white key contours at the edge of the picture if not present
+	int avg_white_lower_y = 0;
+	for (int i = 0, n = boundRect.size(); i < n; i++)
+		avg_white_lower_y += (boundRect[i].y + boundRect[i].height);
+	avg_white_lower_y /= boundRect.size();
+	if (boundRect[0].x > s.width/100) 
+		modifyContours(0, 0, 0, avg_white_lower_y, contours, boundRect, mu, mc, true);
+	if (boundRect[boundRect.size() - 1].x < 99*s.width/100)
+		modifyContours(contours.size(), s.width - 1, 0, avg_white_lower_y, contours, boundRect, mu, mc, true);
+
+
+	// getting median in y coordinate of edges detected and average deviation from median
+	vector<int> u_y, l_y, dist;
+	for (int i = 0, n = contours.size(); i < n; i++) {
+		u_y.push_back(boundRect[i].y);
+		l_y.push_back(boundRect[i].y + boundRect[i].height);
+	}
+	for (int i = 0, n = contours.size(); i < n - 1; i++)
+		dist.push_back(mc[i + 1].x - mc[i].x);
+
+	int u_y_median = median(u_y);
+	int l_y_median = median(l_y);
+	int dist_median = median(dist); 
+	int u_y_error_median = medianDev(u_y, u_y_median);
+	int l_y_error_median = medianDev(l_y, l_y_median);
+
+
+	// correcting detected edges vertically
+	for (int i = 0, n = contours.size(); i < n; i++)
+		if (abs(boundRect[i].y - u_y_median) > 10*u_y_error_median || abs(boundRect[i].y + boundRect[i].height - l_y_median) > 10*l_y_error_median)	
+			modifyContours(i, mc[i].x, u_y_median, l_y_median, contours, boundRect, mu, mc, false);
+
+
+	// adding undetected edges of white keys
+	int i = 0;
+	while (i < n_white_keys) {
+		if (abs(mc[i + 1].x - mc[i].x) > 3.0*dist_median/2)
+			modifyContours(i + 1, mc[i].x + dist_median, u_y_median, l_y_median, contours, boundRect, mu, mc, true);
+		i++;
+	}
+}
+
+
+void getBlackKeys(Mat &bg, vvP &black_keys, vector<Rect> &black_keys_boundRect) {
 	
 	Size s = bg.size();
 	Mat bg_gray;				// grayscale bg image
@@ -170,14 +186,16 @@ void get_black_keys(Mat &bg, vvP &black_keys, vector<Rect> &black_keys_boundRect
 	}
 
 	// sorting to get black keys from left to right
-	sort_contours(contours, boundRect);
+	sortContours(contours, boundRect);
 	
 	black_keys = contours;
 	black_keys_boundRect = boundRect;
 
 }
 
-void transfer_points(vector<Point> &dest, vector<Point> &src, float x_threshold, bool left_side) {
+
+// getting contour of white keys from contours of already detected black keys
+void transferPoints(vector<Point> &dest, vector<Point> &src, float x_threshold, bool left_side) {
 	int side = left_side ? -1 : 1;
 	int j = 0;
 	while (j < src.size() && side*src[j].x > side*x_threshold)
@@ -200,7 +218,7 @@ void transfer_points(vector<Point> &dest, vector<Point> &src, float x_threshold,
 	}
 }
 
-void get_white_keys(Mat &bg, vvP &black_keys, vector<Rect> &black_keys_boundRect, vvP &white_keys) {
+void getWhiteKeys(Mat &bg, vvP &black_keys, vector<Rect> &black_keys_boundRect, vvP &white_keys) {
 
 	Size s = bg.size();
 
@@ -245,27 +263,16 @@ void get_white_keys(Mat &bg, vvP &black_keys, vector<Rect> &black_keys_boundRect
 	vector<Rect> boundRect;
 	get_bddRect(contours, contours_poly, boundRect);
 
-	// removing false positive white keys detected
-	Mat bg_lower_contour = bg.clone();
-	for (int i = 0, n = contours.size(), j = 0; i < n; i++) {
-		if (boundRect[j].height < s.height/6 || boundRect[j].width > s.width/10) {
-			boundRect.erase(boundRect.begin() + j);
-			contours.erase(contours.begin() + j);
-		}
-		else
-			j++;
-	}
-	sort_contours(contours, boundRect);	
-
 	// getting positions where there is no black key between two white keys
 	vector<int> gap_pos;
-	get_gap_pos(black_keys_boundRect, gap_pos);
+	gapPosition(black_keys_boundRect, gap_pos);
 
+	// centers of detected contours
 	vector<Moments> mu(contours.size());
 	vector<Point2f> mc(contours.size());
 
 	// checking if white key edges are properly detected and correcting if necessary
-	modify_detected(contours, boundRect, mu, mc, black_keys.size() + gap_pos.size(), bg);
+	refineDetected(contours, boundRect, mu, mc, black_keys.size() + gap_pos.size(), bg);
 
 	// polygon approximation of contours (can try convexHull)
 	vvP black_keys_poly(black_keys.size());
@@ -284,7 +291,7 @@ void get_white_keys(Mat &bg, vvP &black_keys, vector<Rect> &black_keys_boundRect
 			white_keys[i].push_back(Point(mc[i].x, boundRect[i].y));
 			white_keys[i].push_back(Point(mc[i].x, boundRect[i].y + boundRect[i].height));
 			
-			transfer_points(white_keys[i], black_keys_poly[k - 1], mc[i].x, false);
+			transferPoints(white_keys[i], black_keys_poly[k - 1], mc[i].x, false);
 
 			white_keys[i].push_back(Point(mc[i + 1].x, s.height));
 			white_keys[i].push_back(Point(mc[i + 1].x, boundRect[i + 1].y));
@@ -295,7 +302,7 @@ void get_white_keys(Mat &bg, vvP &black_keys, vector<Rect> &black_keys_boundRect
 			white_keys[i].push_back(Point(mc[i].x, boundRect[i].y));
 			white_keys[i].push_back(Point(mc[i].x, s.height));
 			
-			transfer_points(white_keys[i], black_keys_poly[k], mc[i + 1].x, true);
+			transferPoints(white_keys[i], black_keys_poly[k], mc[i + 1].x, true);
 			
 			white_keys[i].push_back(Point(mc[i + 1].x, boundRect[i + 1].y + boundRect[i + 1].height));
 			white_keys[i].push_back(Point(mc[i + 1].x, boundRect[i + 1].y));
@@ -306,9 +313,9 @@ void get_white_keys(Mat &bg, vvP &black_keys, vector<Rect> &black_keys_boundRect
 			white_keys[i].push_back(Point(mc[i].x, boundRect[i].y));
 			white_keys[i].push_back(Point(mc[i].x, boundRect[i].y + boundRect[i].height));
 			
-			transfer_points(white_keys[i], black_keys_poly[k - 1], mc[i].x, false);
+			transferPoints(white_keys[i], black_keys_poly[k - 1], mc[i].x, false);
 
-			transfer_points(white_keys[i], black_keys_poly[k], mc[i + 1].x, true);
+			transferPoints(white_keys[i], black_keys_poly[k], mc[i + 1].x, true);
 			
 			white_keys[i].push_back(Point(mc[i + 1].x, boundRect[i + 1].y + boundRect[i + 1].height));
 			white_keys[i].push_back(Point(mc[i + 1].x, boundRect[i + 1].y));
@@ -331,11 +338,11 @@ int main(int argc, char** argv) {
 	vector<Rect> black_keys_boundRect;
 	vector<int> gap_pos;
 	
-	get_black_keys(bg, black_keys, black_keys_boundRect);
-	get_gap_pos(black_keys_boundRect, gap_pos);
+	getBlackKeys(bg, black_keys, black_keys_boundRect);
+	gapPosition(black_keys_boundRect, gap_pos);
 
 	vvP white_keys;
-	get_white_keys(bg, black_keys, black_keys_boundRect, white_keys);
+	getWhiteKeys(bg, black_keys, black_keys_boundRect, white_keys);
 
 	draw_contours_show(bg, black_keys, true);
 	draw_contours_show(bg, white_keys, true);
